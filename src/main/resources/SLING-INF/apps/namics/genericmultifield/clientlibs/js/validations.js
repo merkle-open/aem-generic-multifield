02931f623d1c9c10268372fb77df94ba4dedd3c6
Alexandru Suciu <suciua@gmail.com>
2018-03-20 16:05:33 +0100

    Added "required" validator
    The "minElements" validation didn't work for the field visibility because
    it validated the generic multifield even if hidden.

--- src/main/resources/SLING-INF/apps/namics/genericmultifield/clientlibs/js/validations.js
+++ src/main/resources/SLING-INF/apps/namics/genericmultifield/clientlibs/js/validations.js
@@ -3,87 +3,117 @@
  * restriction.
  */
 
-(function(window, $, CUI) {
-	"use strict";
+(function (window, $, CUI) {
+    "use strict";
 
-	/**
-	 * Performs the validation of the generic multifield
-	 */
-	function performValidation(el) {
-		var api = el.adaptTo("foundation-validation");
-		if (api) {
-			api.checkValidity();
-			api.updateUI();
-		}
-	}
+    /**
+     * Performs the validation of the generic multifield
+     */
+    function performValidation(el) {
+        var api = el.adaptTo("foundation-validation");
+        if (api) {
+            api.checkValidity();
+            api.updateUI();
+        }
+    }
 
-	// get global foundation registry
-	var registry = $(window).adaptTo("foundation-registry");
+    // get global foundation registry
+    var registry = $(window).adaptTo("foundation-registry");
 
-	// register selector for generic multifield
-	registry.register("foundation.validation.selector", {
-	  submittable : ".coral-GenericMultiField",
-	  candidate : ".coral-GenericMultiField:not([disabled]):not([data-renderreadonly=true])",
-	  exclusion : ".coral-GenericMultiField *"
-	});
+    // register adapter for generic multifield
+    registry.register("foundation.adapters", {
+        type: "foundation-field",
+        selector: ".coral-GenericMultiField",
+        adapter: function (el) {
+            var $el = $(el);
 
-	var FIELD_ERROR_KEY = "coral-validations.internal.field.error";
-	var fieldErrorEl = $(document.createElement("span")).addClass("coral-Form-fielderror coral-Icon coral-Icon--alert coral-Icon--sizeS")
-	    .attr({
-	      "data-init" : "quicktip",
-	      "data-quicktip-type" : "error"
-	    });
+            return {
+                getName: function () {
+                    return $el.data("name");
+                },
+                setName: function (name) {
+                    $el.data("name", name);
+                },
+                isDisabled: function () {
+                    return !!$el.attr("disabled");
+                },
+                setDisabled: function (disabled) {
+                    if (disabled === true) {
+                        $el.attr("disabled", "disabled");
+                    }
+                },
+                isInvalid: function () {
+                    return $el.attr("aria-invalid") === "true";
+                },
+                setInvalid: function (invalid) {
+                    $el.attr("aria-invalid", !!invalid ? "true" : "false").toggleClass("is-invalid", invalid);
+                },
+                isRequired: function () {
+                    return $el.attr("aria-required") === "true";
+                },
+                setRequired: function (required) {
+                    $el.attr("aria-required", !!required ? "true" : "false");
+                }
+            };
+        }
+    });
 
-	// register validator for generic multifield
-	registry.register("foundation.validation.validator", {
-	  selector : ".coral-GenericMultiField",
-	  validate : function(el) {
-		  var field = $(el.closest(".coral-Form-field")), items = field.find(".coral-GenericMultiField-list li"), minElements = field
-		      .data("minelements"), maxElements = field.data("maxelements");
+    // register selector for generic multifield
+    registry.register("foundation.validation.selector", {
+        submittable: ".coral-GenericMultiField",
+        candidate: ".coral-GenericMultiField:not([disabled]):not([data-renderreadonly=true])",
+        exclusion: ".coral-GenericMultiField *"
+    });
 
-		  // validate if minElements restriction is met
-		  if (items && !isNaN(minElements) && items.length < minElements) {
-			  return Granite.I18n.get('At least {0} items must be created', minElements);
-		  }
-		  // validate if maxElements restriction is met
-		  if (items && !isNaN(maxElements) && items.length > maxElements) {
-			  return Granite.I18n.get('At most {0} items can be created', maxElements);
-		  }
+    var FIELD_ERROR_KEY = "coral-validations.internal.field.error";
+    var fieldErrorEl = $(document.createElement("span")).addClass("coral-Form-fielderror coral-Icon coral-Icon--alert coral-Icon--sizeS")
+        .attr({
+            "data-init": "quicktip",
+            "data-quicktip-type": "error"
+        });
 
-		  return null;
-	  },
-	  show : function(el, message, ctx) {
-	    var fieldErrorEl, $field, error, arrow;
+    // register validator for generic multifield
+    registry.register("foundation.validation.validator", {
+        selector: ".coral-GenericMultiField",
+        validate: function (el) {
+            var $field = $(el).closest(".coral-Form-field"), items = $field.find(".coral-GenericMultiField-list li"), minElements = $field
+                .data("minelements"), maxElements = $field.data("maxelements");
 
-      fieldErrorEl = $("<span class='coral-Form-fielderror coral-Icon coral-Icon--alert coral-Icon--sizeS' data-init='quicktip' data-quicktip-type='error' />");
-      $field = $(el).closest(".coral-Form-field");
+            // validate required attribute
+            if ($field.adaptTo("foundation-field").isRequired() && items.length == 0) {
+                return Granite.I18n.get("Please fill out this field.");
+            }
 
-      $field.attr("aria-invalid", "true").toggleClass("is-invalid", true);
-      $field.nextAll(".coral-Form-fieldinfo").addClass("u-coral-screenReaderOnly");
+            // validate min and max elements
+            if ($field.adaptTo("foundation-field").isRequired() || items.length > 0) {
+                // validate if minElements restriction is met
+                if (items && !isNaN(minElements) && items.length < minElements) {
+                    return Granite.I18n.get('At least {0} items must be created', minElements);
+                }
+                // validate if maxElements restriction is met
+                if (items && !isNaN(maxElements) && items.length > maxElements) {
+                    return Granite.I18n.get('At most {0} items can be created', maxElements);
+                }
+            }
 
-      error = $field.nextAll(".coral-Form-fielderror");
+            return null;
+        },
+        show: function (el, message, ctx) {
+            var $field = $(el).closest(".coral-Form-field");
+            $field.adaptTo("foundation-field").setInvalid(true);
+            ctx.next();
+        },
+        clear: function (el, ctx) {
+            var $field = $(el).closest(".coral-Form-field");
+            $field.adaptTo("foundation-field").setInvalid(false);
+            $field.siblings(".coral-Icon--alert").remove();
+            ctx.next();
+        }
+    });
 
-      if (error.length === 0) {
-        arrow = $field.closest("form").hasClass("coral-Form--vertical") ? "right" : "top";
-
-        fieldErrorEl.attr("data-quicktip-arrow", arrow).attr("data-quicktip-content", message).insertAfter($field);
-      } else {
-        error.data("quicktipContent", message);
-      }
-	  },
-	  clear : function(el, ctx) {
-	  	var $field = $(el).closest(".coral-Form-field");
-
-	  	$field.removeAttr("aria-invalid").removeClass("is-invalid");
-
-	  	$field.nextAll(".coral-Form-fielderror").tooltip("hide").remove();
-	  	$field.nextAll(".coral-Form-fieldinfo").removeClass("u-coral-screenReaderOnly");
-	  }
-	});
-
-	// perform validation every time generic multifield changed
-	$(document).on("change", ".coral-GenericMultiField", function(e) {
-		performValidation($(this));
-	});
+    // perform validation every time generic multifield changed
+    $(document).on("change", ".coral-GenericMultiField", function() {
+        performValidation($(this));
+    });
 
 })(window, Granite.$, CUI);
